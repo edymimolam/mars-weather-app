441cb6f8179fc873936c520a315848b852fb05a4
edymimolam <edymimolam@gmail.com>
2020-04-26 15:46:18 +0300

    add useNasaApi

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import CurrentWeather from "./components/CurrentWeather";
 import PreviousWeather from "./components/PreviousWeather";
 import { Theme, GlobalStyle } from "./style";
+import { useNasaApi } from "./useNasaApi";
 
-const App = () => (
-  <Theme>
-    <GlobalStyle />
-    <CurrentWeather />
-    <PreviousWeather />
-  </Theme>
-);
+const App = () => {
+  let { isLoading, sols, setSols } = useNasaApi();
+  console.log("App -> isLoading, sols", isLoading, sols);
+
+  return (
+    <Theme>
+      <GlobalStyle />
+      <CurrentWeather />
+      <PreviousWeather />
+    </Theme>
+  );
+};
 
 export default App;

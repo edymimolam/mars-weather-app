44dcc5b74c04ad8adad633a1e98282f126827a87
edymimolam <edymimolam@gmail.com>
2020-04-27 20:21:29 +0300

    add mockData

--- src/useNasaApi.js
+++ src/useNasaApi.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { createSols } from "./createSols";
+import data from "./mockData";
 
 const URL =
   "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0";
@@ -9,14 +10,19 @@ export function useNasaApi(setCurrentSol) {
   let [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch(URL)
-      .then((response) => response.json())
-      .then((data) => {
-        let _sols = createSols(data);
-        setSols(_sols);
-        setCurrentSol(_sols[_sols.length - 1]);
-        setIsLoading(false);
-      });
+    // fetch(URL)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     let _sols = createSols(data);
+    //     setSols(_sols);
+    //     setCurrentSol(_sols[_sols.length - 1]);
+    //     setIsLoading(false);
+    //   });
+
+    let _sols = createSols(data);
+    setSols(_sols);
+    setCurrentSol(_sols[_sols.length - 1]);
+    setIsLoading(false);
   }, [setCurrentSol]);
 
   return { isLoading, sols };

9450b420a55f604965375d54ac689365d2bf7b6f
edymimolam <edymimolam@gmail.com>
2020-04-28 15:58:44 +0300

    Updates

---
+++ precache-manifest.91038055b1ac9efea6e72242d7de462e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8421ee8c08046f61cf40487c21bdc22d",
+    "url": "/mars-weather-app/index.html"
+  },
+  {
+    "revision": "80b8c753991c79615473",
+    "url": "/mars-weather-app/static/js/2.dd659939.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mars-weather-app/static/js/2.dd659939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef6f113439f7c3533420",
+    "url": "/mars-weather-app/static/js/main.f206ffe4.chunk.js"
+  },
+  {
+    "revision": "bdf14d381f3650cd4557",
+    "url": "/mars-weather-app/static/js/runtime-main.c2cb2c40.js"
+  },
+  {
+    "revision": "afe4aa06c05fe3c33fe1e3e94a1c7469",
+    "url": "/mars-weather-app/static/media/mars.afe4aa06.jpg"
+  }
+]);

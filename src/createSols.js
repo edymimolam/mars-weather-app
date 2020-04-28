d5f9fc313c8d1075a359699687d4d001d1b0a5c1
edymimolam <edymimolam@gmail.com>
2020-04-28 15:19:55 +0300

    add checks for required data in useNasaApi

--- src/createSols.js
+++ src/createSols.js
@@ -3,22 +3,24 @@ export const createSols = (data) =>
     solNum: key,
     date: formatDate(data[key].First_UTC),
     temperature: {
-      metricHigh: Math.round(data[key].AT.mx),
-      metricLow: Math.round(data[key].AT.mn),
-      imperialHigh: cToF(data[key].AT.mx),
-      imperialLow: cToF(data[key].AT.mn),
+      metricHigh: Math.round(+!!data[key].AT && data[key].AT.mx),
+      metricLow: Math.round(+!!data[key].AT && data[key].AT.mn),
+      imperialHigh: cToF(+!!data[key].AT && data[key].AT.mx),
+      imperialLow: cToF(+!!data[key].AT && data[key].AT.mn),
     },
     wind: {
-      metricSpeed: mPerSToKmPerH(data[key].HWS.av),
-      imperialSpeed: mPerSToMilesPerH(data[key].HWS.av),
-      compassDegrees: Math.round(data[key].WD.most_common.compass_degrees),
+      metricSpeed: mPerSToKmPerH(+!!data[key].HWS && data[key].HWS.av),
+      imperialSpeed: mPerSToMilesPerH(+!!data[key].HWS && data[key].HWS.av),
+      compassDegrees: Math.round(
+        +!!data[key].WD && data[key].WD.most_common.compass_degrees
+      ),
     },
   }));
 
-const cToF = (c) => Math.round((c * 9) / 5 + 32);
+const cToF = (c = 0) => Math.round((c * 9) / 5 + 32);
 
-const mPerSToKmPerH = (mPerS) => Math.round(mPerS * 3.6);
-const mPerSToMilesPerH = (mPerS) => Math.round(mPerS * 3.6 * 0.61);
+const mPerSToKmPerH = (mPerS = 0) => Math.round(mPerS * 3.6);
+const mPerSToMilesPerH = (mPerS = 0) => Math.round(mPerS * 3.6 * 0.61);
 
 const formatDate = (firstDate) => {
   let date = new Date(firstDate);

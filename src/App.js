77ed36a876ca2403890fa698bc3b484389f39c05
edymimolam <edymimolam@gmail.com>
2020-05-08 19:36:53 +0300

    move createSols from useNasaApi to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { LoadingSpinner } from "./components/common";
 import CurrentWeather from "./components/CurrentWeather";
 import PreviousWeather from "./components/PreviousWeather";
 import { Theme, GlobalStyle } from "./style";
 import { useNasaApi } from "./useNasaApi";
+import { createSols } from "./createSols";
 
 const App = () => {
+  let [isLoading, setIsLoading] = useState(true);
+  let [sols, setSols] = useState(null);
   let [currentSol, setCurrentSol] = useState(null);
-  let { isLoading, sols } = useNasaApi(setCurrentSol);
   let [isMetric, setIsMetric] = useState(true);
+
+  let data = useNasaApi();
+
+  useEffect(() => {
+    if (data) {
+      setSols(createSols(data));
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (sols) {
+      setCurrentSol(sols[sols.length - 1]);
+      setIsLoading(false);
+    }
+  }, [sols]);
 
   const onUnitClick = () => setIsMetric((isMetric) => !isMetric);
   const onMoreInfoClick = (solNum) => {
--- src/useNasaApi.js
+++ src/useNasaApi.js
@@ -1,23 +1,18 @@
 import { useEffect, useState } from "react";
-import { createSols } from "./createSols";
 
 const URL =
   "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0";
 
-export function useNasaApi(setCurrentSol) {
-  let [sols, setSols] = useState(null);
-  let [isLoading, setIsLoading] = useState(true);
+export function useNasaApi() {
+  let [data, setData] = useState(null);
 
   useEffect(() => {
     fetch(URL)
       .then((response) => response.json())
-      .then((data) => {
-        let _sols = createSols(data);
-        setSols(_sols);
-        setCurrentSol(_sols[_sols.length - 1]);
-        setIsLoading(false);
+      .then((json) => {
+        setData(json);
       });
-  }, [setCurrentSol]);
+  }, []);
 
-  return { isLoading, sols };
+  return data;
 }
